eaa6f4979e70cfd9a7f53c8b29957faaf5256978
anna757 <hanan.korabi@stud.hshl.de>
2023-06-19 14:45:52 +0200

    works for all variants now

--- updateStock.py
+++ updateStock.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import shopify
-
+import time
 """
 Update the inventory level of a given product's variants on Shopify based on the available sizes provided.
 
@@ -30,20 +30,23 @@ def update_shopify_inventory(product_id, out_of_stock_sizes, color):
     # For each variant, if the color matches and the size is in the list of out-of-stock sizes, set the inventory level to 0
     # Otherwise, do not change the inventory level
     for variant in variants:
-        if variant.option1 == color:
-            # Get the current inventory level for the variant
-            inventory_item_id = variant.inventory_item_id
-            inventory_level = shopify.InventoryLevel.find(inventory_item_ids=inventory_item_id, location_ids=location_id)[0]
+        time.sleep(0.5)
+        # Get the current inventory level for the variant
+        inventory_item_id = variant.inventory_item_id
+        inventory_level = shopify.InventoryLevel.find(inventory_item_ids=inventory_item_id, location_ids=location_id)[0]
+        
+        variant_color = variant.option1
+        variant_size = variant.option2 if color else variant.option1
 
-            if variant.option2 in out_of_stock_sizes:
-                print("Out of stock: ", variant.option1, variant.option2)
-                new_level = 0
-            else:
-                print("Available: ", variant.option1, variant.option2)
-                new_level = 10  # or whatever number you want
-
-            # Update the inventory level
-            inventory_level.set(location_id, inventory_item_id, new_level)
+        new_level = 10
+        if color and variant_color == color and variant_size in out_of_stock_sizes:
+            print("Out of stock: ", color, variant_size)
+            new_level = 0
+        elif not color and variant_size in out_of_stock_sizes:
+            print("Out of stock: ", variant_size)
+            new_level = 0
+        # Update the inventory level
+        inventory_level.set(location_id, inventory_item_id, new_level)
 
 
 """
@@ -85,8 +88,8 @@ None
 def main():
     # List of Trendyol product URLs and corresponding Shopify product IDs
     products = [
-        {"url": "https://www.trendyol.com/trendyolmilla/yesil-vucuda-oturan-dokuma-sik-abiye-elbise-tprss23el00280-p-380870752", "product_id": "8441935724871", "color":"green"},
         {"url": "https://www.trendyol.com/trendyolmilla/lila-vucuda-oturan-dokuma-sik-abiye-elbise-tprss23el00280-p-380870751", "product_id": "8441935724871", "color": "lilac"},
+        {"url": "https://www.trendyol.com/trendyolmilla/yesil-vucuda-oturan-dokuma-sik-abiye-elbise-tprss23el00280-p-380870752", "product_id": "8441935724871", "color":"green"},
         {"url": "https://www.trendyol.com/trendyolmilla/cok-renkli-dokuma-saten-cicek-desenli-sik-abiye-elbise-tprss23el00305-p-389601623?boutiqueId=620391&merchantId=968", "product_id": "8444920627527", "color":""},
         {"url": "https://www.trendyol.com/trendyolmilla/siyah-buzgulu-orme-sik-abiye-elbise-tprss22el1757-p-196139101", "product_id": "8444930752839", "color":""},
         {"url": "https://www.trendyol.com/trendyolmilla/ekru-belden-acilan-skater-astarli-brode-bridal-sik-abiye-elbise-tprss22el1011-p-187393467?boutiqueId=621324&merchantId=968", "product_id": "8444933341511", "color":""},
@@ -99,6 +102,7 @@ def main():
         out_of_stock_sizes = scrape_trendyol_page(product["url"])
         #print(product, out_of_stock_sizes)
         update_shopify_inventory(product["product_id"], out_of_stock_sizes, product["color"])
+        
         
 # Test the function with one of your product URLs
 
